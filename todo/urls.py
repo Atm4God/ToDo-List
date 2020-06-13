43fb3a80ecc3311206c7d48101360084ebb1cb13
Marvellous <tmarvellous41@gmail.com>
2020-06-13 19:11:04 +0100

    initial commit

---
+++ todo/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add', views.add_todo, name = 'add'),
+    path('complete/<todo_id>', views.complete_todo, name='complete'),
+    path('delete_complete', views.delete_completed, name='delete_complete'),
+    path('delete_all', views.delete_all, name='delete_all'),
+
+] 
